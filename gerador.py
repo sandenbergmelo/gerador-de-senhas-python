d300a592b83bb3155e29080303e2f2ef1d579c84
Sandenberg <sandenbergferreiramelo@gmail.com>
2022-02-01 10:00:37 -0300

    Tabs trocados para espaços

--- gerador.py
+++ gerador.py
@@ -3,21 +3,21 @@ from gerador_cli import gerar_senha, salvar_senha
 
 # Funções
 def limpar_saida(obj):
-	janela.FindElement(obj).Update('')
+    janela.FindElement(obj).Update('')
 
 # Layout
 layout = [
-	[sg.Text('Quantidade de letras: '),
-	sg.Combo(values=list(range(17)), default_value=8, key='letras')],
-	[sg.Text('Quantidade de números: '),
-	sg.Combo(values=list(range(17)), default_value=4, key='numeros')],
-	[sg.Text('Quantidade de caracteres especiais: '),
-	sg.Combo(values=list(range(17)), default_value=2, key='chars')],
-	[sg.Checkbox('Salvar senha', key='salvar')],
-	[sg.Button('Gerar Senha', key='gerar', button_color='#008020'),
-	sg.Button('Limpar', key='limpar', button_color='#07f'),
-	sg.Button('Sair', key='sair', button_color='gray')],
-	[sg.Output(key='resultado', size=(36, 10), font='Consolas')]
+    [sg.Text('Quantidade de letras: '),
+    sg.Combo(values=list(range(17)), default_value=8, key='letras')],
+    [sg.Text('Quantidade de números: '),
+    sg.Combo(values=list(range(17)), default_value=4, key='numeros')],
+    [sg.Text('Quantidade de caracteres especiais: '),
+    sg.Combo(values=list(range(17)), default_value=2, key='chars')],
+    [sg.Checkbox('Salvar senha', key='salvar')],
+    [sg.Button('Gerar Senha', key='gerar', button_color='#008020'),
+    sg.Button('Limpar', key='limpar', button_color='#07f'),
+    sg.Button('Sair', key='sair', button_color='gray')],
+    [sg.Output(key='resultado', size=(36, 10), font='Consolas')]
 ]
 
 # Janela
@@ -25,22 +25,22 @@ janela = sg.Window('Gerador de senhas', layout=layout)
 
 # Eventos
 while True:
-	evento, valores = janela.read()
+    evento, valores = janela.read()
 
-	if evento == sg.WIN_CLOSED or evento == 'sair':
-		break
+    if evento == sg.WIN_CLOSED or evento == 'sair':
+        break
 
-	if evento == 'limpar':
-		limpar_saida('resultado')
-	
-	if evento == 'gerar':
-		letras, numeros, chars = valores['letras'], valores['numeros'], valores['chars']
+    if evento == 'limpar':
+        limpar_saida('resultado')
 
-		if letras != 0 or numeros != 0 or chars != 0:
-			senha = gerar_senha(letras, numeros, chars)
-			print(senha)
+    if evento == 'gerar':
+        letras, numeros, chars = valores['letras'], valores['numeros'], valores['chars']
 
-			if valores['salvar']:
-				sg.popup(salvar_senha(senha), title='Salvar senha')
-		else:
-			sg.popup('Impossível gerar senha vazia!', title='Erro!')
+        if letras != 0 or numeros != 0 or chars != 0:
+            senha = gerar_senha(letras, numeros, chars)
+            print(senha)
+
+            if valores['salvar']:
+                sg.popup(salvar_senha(senha), title='Salvar senha')
+        else:
+            sg.popup('Impossível gerar senha vazia!', title='Erro!')
--- gerador_cli.py
+++ gerador_cli.py
@@ -5,47 +5,47 @@ symbols  = '@#.,*%+=-!?&'
 
 # Funções
 def gerar_senha(letras=8, numeros=4, caracteres_especiais=2):
-	generated = ''
-	generated += gerar_caracteres(letras, ascii_letters)
-	generated += gerar_caracteres(numeros, digits)
-	generated += gerar_caracteres(caracteres_especiais, symbols)
-	
-	senha = embaralhar_caracteres(generated)
-	return senha
+    generated = ''
+    generated += gerar_caracteres(letras, ascii_letters)
+    generated += gerar_caracteres(numeros, digits)
+    generated += gerar_caracteres(caracteres_especiais, symbols)
+
+    senha = embaralhar_caracteres(generated)
+    return senha
 
 def gerar_caracteres(length, chars):
-	generated = ''
-	for _ in range(length):
-		generated += choice(chars)
-	return generated
+    generated = ''
+    for _ in range(length):
+        generated += choice(chars)
+    return generated
 
 def embaralhar_caracteres(chars):
-	chars = list(chars)
-	shuffle(chars)
-	return ''.join(chars)
+    chars = list(chars)
+    shuffle(chars)
+    return ''.join(chars)
 
 def salvar_senha(senha):
-	with open('senhas.txt', 'a') as senhas:
-		senhas.write(f'{senha}\n')
-	return 'Senha salva em senhas.txt'
+    with open('senhas.txt', 'a') as senhas:
+        senhas.write(f'{senha}\n')
+    return 'Senha salva em senhas.txt'
 
 if __name__ == '__main__':
-	from argparse import ArgumentParser
-	
-	parser = ArgumentParser()
-	parser.add_argument('--letras', '-l',type=int, default=8, help='Quantidade de letras')
-	parser.add_argument('--numeros', '-n',type=int, default=4, help='Quantidade de números')
-	parser.add_argument('--chars', '-c',type=int, default=2, help='Quantidade de caracteres especiais')
-	parser.add_argument('--salvar', '-s', type=bool, nargs='?', default=False, help='Salvar senha em arquivo txt')
-	
-	args = parser.parse_args()
+    from argparse import ArgumentParser
 
-	if args.salvar != False:
-		args.salvar = True
-	
-	senha = gerar_senha(args.letras, args.numeros, args.chars)
-	
-	print(senha)
+    parser = ArgumentParser()
+    parser.add_argument('--letras', '-l',type=int, default=8, help='Quantidade de letras')
+    parser.add_argument('--numeros', '-n',type=int, default=4, help='Quantidade de números')
+    parser.add_argument('--chars', '-c',type=int, default=2, help='Quantidade de caracteres especiais')
+    parser.add_argument('--salvar', '-s', type=bool, nargs='?', default=False, help='Salvar senha em arquivo txt')
 
-	if args.salvar:
-		print(salvar_senha(senha))
+    args = parser.parse_args()
+
+    if args.salvar != False:
+        args.salvar = True
+
+    senha = gerar_senha(args.letras, args.numeros, args.chars)
+
+    print(senha)
+
+    if args.salvar:
+        print(salvar_senha(senha))

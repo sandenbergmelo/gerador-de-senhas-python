762128cee6c50032a82c1b3f2c9b6d6b94f844f1
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-03-05 09:27:00 -0300

    Refatoração

--- gerador.py
+++ gerador.py
@@ -1,32 +1,35 @@
 from PyQt5 import QtWidgets, uic
 from gerador_cli import gerar_senha
 from platform import system
-from ui.pop_up import pop_up
+from gui.pop_up import pop_up
 
 def salvar_senha(senha):
 
+    # Opções de salvamento
     options = QtWidgets.QFileDialog.Options()
     options |= QtWidgets.QFileDialog.DontConfirmOverwrite
     if system() == 'Linux':
         options |= QtWidgets.QFileDialog.DontUseNativeDialog
 
-    arquivo = QtWidgets.QFileDialog.getSaveFileName(
+    arquivo = QtWidgets.QFileDialog.getSaveFileName(# Abre janela de salvamento
         None,
         caption='Salvar senha',
         directory='senhas.txt',
         filter='Arquivo de texto (*.txt);;Todos os arquivos (*)',
         options=options
     )[0]
-
-    if arquivo == '':
+    
+    if arquivo == '': # Se o usuário não selecionou um arquivo
         return False
     
+    # Salva a senha no arquivo
     with open(arquivo, 'a') as senhas:
             senhas.write(f'{senha}\n')
     return True
 
 def main():
     
+    # Pega os valores dos campos
     letras = int(tela.spinLetras.value())
     numeros = int(tela.spinNumeros.value())
     caracteres = int(tela.spinCaracteres.value())
@@ -34,11 +37,12 @@ def main():
     if letras != 0 or numeros != 0 or caracteres != 0:
         senha = gerar_senha(letras, numeros, caracteres)
 
+        # Mostra a senha na tela
         saida = tela.listSaida
         saida.addItem(senha)
         saida.scrollToBottom()
 
-        if tela.checkSalvarSenha.isChecked():
+        if tela.checkSalvarSenha.isChecked():# Se o usuário quer salvar a senha
             salvo = salvar_senha(senha)
             if salvo:
                 pop_up(
@@ -59,11 +63,15 @@ def main():
             'Impossível gerar senha vazia!'
         )
 
+# Inicialização do programa
 app = QtWidgets.QApplication([])
-tela = uic.loadUi("ui/tela.ui")
+tela = uic.loadUi("gui/ui/tela.ui")
+
+# Eventos dos botões da tela
 tela.pushGerarSenha.clicked.connect(main)
 tela.pushLimpar.clicked.connect(tela.listSaida.clear)
 tela.pushSair.clicked.connect(app.quit)
 
+# Exibir tela
 tela.show()
 app.exec()

3f06603c71675aeefe00b4c6f497c515001ffecf
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-09-14 08:43:29 -0300

    Init migration

--- gui/pop_up.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-
-def pop_up(titulo, icone, texto):
-    msg = QtWidgets.QMessageBox()
-    msg.setWindowTitle(titulo)
-    msg.setIcon(icone)
-    msg.setText(texto)
-    msg.setStyleSheet('color: #f8f8f2; background-color: #282a36;')
-    msg.exec()
---
+++ utils/pop_up.py
@@ -0,0 +1,15 @@
+from PySide6.QtWidgets import QMessageBox
+
+
+def pop_up(title, txt, icon='information'):
+    icons = {'information': QMessageBox.Information,
+             'warning': QMessageBox.Warning,
+             'critical': QMessageBox.Critical,
+             'question': QMessageBox.Question}
+
+    msg = QMessageBox()
+    msg.setWindowTitle(title)
+    msg.setIcon(icons[icon])
+    msg.setText(txt)
+    msg.setStyleSheet('color: #f8f8f2; background-color: #282a36;')
+    msg.exec()

2273e7fcbac560064e66a34b6d809daeae42b4e2
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-04-06 22:06:35 -0300

    Remove unused argv

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from sys import argv
-
 from PySide6.QtWidgets import QApplication
 
 from interface.main_window import MainWindow
 
 
 def main():
-    app = QApplication(argv)
+    app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
